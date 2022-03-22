573e04a8319b33c6a96a3d6ca24ef968c9e447fb
zhanglu <lu.zhang@wengegroup.com>
2022-03-23 00:50:57 +0800

    24-29

---
+++ src/prepare/_26_栈_用栈实现队列.java
@@ -0,0 +1,56 @@
+package prepare;
+
+import java.util.Stack;
+
+/**
+ * @author 张璐
+ * @description &{description} TODO 璐哥 写点注释吧 !!!!!
+ * @date 2022/3/22
+ */
+public class _26_栈_用栈实现队列 {
+	public static class StackQueue<T> {
+		private Stack<T> push = new Stack<>();
+		private Stack<T> pop = new Stack<>();
+
+		public StackQueue() {
+		}
+
+		;
+
+		public void push(T o) {
+			push.push(o);
+		}
+
+		public T pop() {
+			if (pop.isEmpty()) {
+				while (!push.isEmpty()) {
+					pop.push(push.pop());
+				}
+			}
+			if (pop.isEmpty()) {
+				throw new RuntimeException("栈空了");
+			}
+			return pop.pop();
+		}
+	}
+
+	public static void main(String[] args) {
+		StackQueue<Integer> queue = new StackQueue<>();
+		queue.push(1);
+		queue.push(2);
+		queue.push(3);
+		queue.push(4);
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		queue.push(5);
+		queue.push(6);
+		queue.push(7);
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+	}
+}
---
+++ src/prepare/_27_排序_归并排序解决小和问题.java
@@ -0,0 +1,87 @@
+package prepare;
+
+import utils.Utils;
+
+import java.util.Arrays;
+
+/**
+ * @author 张璐
+ * @description &{description} TODO 璐哥 写点注释吧 !!!!!
+ * @date 2022/3/22
+ */
+public class _27_排序_归并排序解决小和问题 {
+	public static int merge(int[] arr, int L, int M, int R) {
+		int leftL = L;
+		int leftR = M - 1;
+		int rightL = M;
+		int rightR = R;
+		int[] help = new int[R - L + 1];
+		int i = 0;
+		int ans = 0;
+		while (leftL <= leftR && rightL <= rightR) {
+			if (arr[rightL] <= arr[leftL]) {
+				help[i++] = arr[rightL++];
+			} else {
+				help[i++] = arr[leftL];
+				ans += arr[leftL] * (rightR - rightL + 1);
+				leftL++;
+			}
+		}
+		while (rightL <= rightR) {
+			help[i++] = arr[rightL++];
+		}
+		while (leftL <= leftR) {
+			help[i++] = arr[leftL++];
+		}
+		for (int j = 0; j < help.length; j++) {
+			arr[j + L] = help[j];
+		}
+		return ans;
+	}
+
+	public static int getSmallSumByMergeSort(int[] arr) {
+//		arr = Arrays.copyOf(arr, arr.length);
+		int ans = 0;
+		int mergeSize = 1;
+		while (mergeSize < arr.length) {
+			for (int i = 0; i < arr.length - mergeSize; i += (mergeSize << 1)) {
+				ans += merge(arr, i, i + mergeSize, Math.min(arr.length - 1, i + 2 * mergeSize - 1));
+			}
+			mergeSize <<= 1;
+		}
+		return ans;
+	}
+
+	public static int check(int[] arr) {
+		int ans = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {
+					ans += arr[j];
+				}
+			}
+		}
+		return ans;
+	}
+
+
+	public static void main(String[] args) {
+//		System.out.println(4 + 3 + 4 + 3 + 5 + 4 + 3 + 5 + 7 + 2 + 4 + 3 + 5 + 2 + 3 + 2);
+		for (int i = 0; i < 10000; i++) {
+			int[] ints = Utils.generateRandomIntArr(1000, 1000);
+//			int[] ints = {4, 3, 5, 7, 2, 8, 6, 4};
+			try {
+				int check = check(ints);
+				int smallSumByMergeSort = getSmallSumByMergeSort(ints);
+				if (smallSumByMergeSort != check) {
+					System.out.println("error");
+					System.out.println(Arrays.toString(ints));
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println(Arrays.toString(ints));
+				break;
+			}
+		}
+	}
+}

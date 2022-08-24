5c29a4ec641bea47cc47ce4d03f45299f3f226fc
张璐 <lu.zhang@wengegroup.com>
2022-08-24 14:06:19 +0800

    loop node

--- src/prepare/_40_链表_寻找环形链表的入环点.java
+++ src/prepare/_40_链表_寻找环形链表的入环点.java
@@ -1,6 +1,7 @@
 package prepare;
 
 import entity.ListNode;
+import entity.RandomListNode;
 
 /**
  * @author 张璐
@@ -26,5 +27,69 @@ public class _40_链表_寻找环形链表的入环点 {
 			slow=slow.next;
 		}
 		return fast;
+	}
+	public static ListNode findLoopNode(ListNode node,int N) {
+		if (node==null||node.next==null) {
+			return null;
+		}
+		ListNode slow= node;
+		ListNode fast= node;
+		int count = 0;
+		loop :do {
+			slow = slow.next;
+			for (int i = 0; i < N; i++) {
+				if (fast == null) {
+					break loop;
+				}
+				fast = fast.next;
+			}
+			count++;
+			if (count>1000) {
+				throw new RuntimeException("ops");
+			}
+		} while (fast != null && fast.next != null && fast != slow);
+
+		if (fast!=slow) {
+			return null;
+		}
+		fast = node;
+		while (fast != slow) {
+			fast=fast.next;
+			slow=slow.next;
+		}
+		return fast;
+	}
+
+	public static ListNode randomListNode(int maxLength) {
+		int length = (int) (Math.random() * (maxLength)) + 1;
+		int loopIndex = (int) (Math.random() * (maxLength + 1));
+		ListNode head =new ListNode();
+		ListNode pre =head;
+		ListNode loop = null;
+		for (int i = 0; i < length; i++) {
+			ListNode cur = new ListNode();
+			if (i==loopIndex) {
+				loop = cur;
+			}
+			pre.next = cur;
+			pre = cur;
+		}
+		pre.next = loop;
+		if (loop!=null) {
+			loop.val = loopIndex;
+		}
+		head.next.val = loopIndex;
+		return head.next;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10000; i++) {
+			ListNode node =randomListNode(10);
+			int val = node.val;
+			ListNode loopNode = findLoopNode(node, 3);
+			if (loopNode != null) {
+				System.out.println(loopNode.val == val);
+			}
+		}
 	}
 }
